86607c7b57a84dcd4488379c05fb3981d7472dbd
Abraham E. Tavarez <abeincwireless@gmail.com>
2024-01-24 16:04:41 -0500

    header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,22 @@
-function Header() {
+
+
+
+function Header(props) {
+  const { user, dept, admin, style, age = 18 } = props;
+
   return (
-    <header>
+    <header style={style}>
       <h1>Sartre's List</h1>
-      <h6>Better-Dressed People</h6>
+      <h2>Better-Dressed People</h2>
+
+      <h3>
+        Hello, {user}, Dept {dept}
+      </h3>
+
+      <h4>{admin ? "Admin" : "Normal User"}</h4>
+
+      <h5>Age: {age}</h5>
     </header>
   );
 }
-export default Header
+export default Header;
